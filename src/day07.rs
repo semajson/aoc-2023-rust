47b5416a814f7e8bc7bf7bfa233ba9cb4480a833
James Sandy <james.sandy@metaswitch.com>
2023-12-08 00:59:19 +0000

    Day 07 - checkpoint

--- src/day07.rs
+++ src/day07.rs
@@ -43,6 +43,7 @@ pub struct Hand {
     threes: Vec<String>,
     pairs: Vec<String>,
     ones: Vec<String>,
+    jokers: bool,
 }
 impl Hand {
     pub fn new(hand: &str, bid: &str) -> Hand {
@@ -54,6 +55,7 @@ impl Hand {
         Hand {
             hand,
             bid,
+            jokers: false,
             hand_type: HandType::Unknown,
             fives: vec![],
             fours: vec![],
@@ -62,26 +64,38 @@ impl Hand {
             ones: vec![],
         }
     }
-    pub fn find_matches(&mut self) {
+    pub fn analyse(&mut self) {
+        self.find_matches();
+        self.set_type();
+    }
+    fn find_matches(&mut self) {
         // SOrt is very important here
         let mut sorted_hand = self.hand.clone();
-        sorted_hand.sort_by(|a, b| strength(b).cmp(&strength(a)));
+        sorted_hand.sort_by(|a, b| a.cmp(&b));
 
-        let mut current_match = sorted_hand[0].to_string();
+        let mut current_match = vec![sorted_hand[0]];
+
         for i in 1..sorted_hand.len() {
-            let char = sorted_hand[i];
-            if current_match.chars().next().unwrap() == char {
-                current_match.push(char)
+            let next_char = sorted_hand[i];
+            if current_match[0] == next_char {
+                current_match.push(next_char)
             } else {
                 self.add_match(current_match);
-                current_match = char.to_string();
+                current_match = vec![next_char];
             }
         }
         self.add_match(current_match);
 
         self.set_type();
     }
-    fn add_match(&mut self, found_match: String) {
+
+    fn add_match(&mut self, found_match: Vec<char>) {
+        let found_match = found_match
+            .iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<String>>()
+            .join("");
+
         match found_match.len() {
             1 => self.ones.push(found_match),
             2 => self.pairs.push(found_match),
@@ -92,22 +106,12 @@ impl Hand {
         }
     }
 
-    // pub fn order_by_matches(&mut self) {
-    //     self.find_matches();
-    //     let hand = vec![
-    //         self.fives.clone(),
-    //         self.fours.clone(),
-    //         self.threes.clone(),
-    //         self.pairs.clone(),
-    //         self.ones.clone(),
-    //     ]
-    //     .concat();
-    //     self.hand = hand.join("").chars().collect();
-    //     self.set_type();
-    // }
-
     fn set_type(&mut self) {
-        self.hand_type = self.get_type();
+        if self.jokers {
+            self.hand_type = self.get_type_jokers();
+        } else {
+            self.hand_type = self.get_type();
+        }
     }
     fn get_type(&self) -> HandType {
         if self.fives.len() == 1 {
@@ -126,6 +130,44 @@ impl Hand {
             HandType::HighCard
         }
     }
+    fn get_type_jokers(&self) -> HandType {
+        let num_jokers = self.hand.iter().filter(|&&x| x == 'J').count();
+
+        if self.fives.len() == 1 {
+            HandType::FiveOfAKind
+        } else if self.fours.len() == 1 {
+            match num_jokers {
+                1 => HandType::FiveOfAKind,
+                _ => HandType::FourOfAKind,
+            }
+        } else if self.threes.len() == 1 && self.pairs.len() == 1 {
+            match num_jokers {
+                0 => HandType::FullHouse,
+                _ => HandType::FiveOfAKind,
+            }
+        } else if self.threes.len() == 1 {
+            match num_jokers {
+                1 => HandType::FourOfAKind,
+                _ => HandType::ThreeOfAKind,
+            }
+        } else if self.pairs.len() == 2 {
+            match num_jokers {
+                2 => HandType::FourOfAKind,
+                1 => HandType::FullHouse,
+                _ => HandType::TwoPair,
+            }
+        } else if self.pairs.len() == 1 {
+            match num_jokers {
+                1 => HandType::ThreeOfAKind,
+                _ => HandType::TwoPair,
+            }
+        } else {
+            match num_jokers {
+                1 => HandType::TwoPair,
+                _ => HandType::HighCard,
+            }
+        }
+    }
 }
 impl PartialEq for Hand {
     fn eq(&self, other: &Hand) -> bool {
@@ -136,17 +178,6 @@ impl Eq for Hand {}
 impl PartialOrd for Hand {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(&other))
-        // if self.hand_type != other.hand_type {
-        //     Some((self.hand_type as isize).cmp(&(other.hand_type as isize)))
-        // } else {
-        //     // Are the same type
-        //     for i in 0..self.hand.len() {
-        //         if self.hand[i] != other.hand[i] {
-        //             return Some(self.hand[i].cmp(&other.hand[i]));
-        //         }
-        //     }
-        //     Some(self.hand[0].cmp(&other.hand[0]));
-        // }
     }
 }
 impl Ord for Hand {
@@ -155,21 +186,16 @@ impl Ord for Hand {
             (self.hand_type as isize).cmp(&(other.hand_type as isize))
         } else {
             // Are the same type
+            // As the spec, iterate through the original string
             for i in 0..self.hand.len() {
                 if self.hand[i] != other.hand[i] {
                     return strength(&self.hand[i]).cmp(&strength(&other.hand[i]));
                 }
             }
-            self.hand[0].cmp(&other.hand[0])
+            self.hand[0].cmp(&other.hand[0]) // todo make equal
         }
     }
 }
-
-// impl Eq for Hand {
-//     fn eq(&self, other: &Hand) -> bool {
-//         self.hand == other.hand
-//     }
-// }
 
 #[derive(Clone, Debug)]
 pub struct Day07;
@@ -193,7 +219,7 @@ impl Solution for Day07 {
     fn part_one(hands: &mut Self::ParsedInput) -> String {
         for hand in hands.iter_mut() {
             // hand.order_by_matches();
-            hand.find_matches()
+            hand.analyse()
         }
         hands.sort();
         // TODO: implement part one
@@ -243,43 +269,6 @@ AA992 1
 AAAA2 1
 AAAA2 1
 AAA22 1"
-            ),
-            "5".to_string()
-        )
-    }
-
-    #[test]
-    fn check_day07_part1_case3() {
-        assert_eq!(
-            Day07::solve_part_one(
-                "AAAA3 1
-8A7J7 301
-QAAT7 677
-J3K4K 622
-KJJ62 577
-49AAA 298
-45585 855
-33KKK 115
-4Q777 438
-7KK77 836
-5T55T 397
-85855 56
-Q6Q38 157
-AA8AA 85
-32J33 293
-KKQQA 247
-888J4 944
-2AJ2K 482
-33777 338
-KT434 696
-K3K63 648
-86866 136
-93983 584
-57857 489
-5JJ2Q 76
-82335 133
-J25T4 559
-9TQ2A 211"
             ),
             "5".to_string()
         )
