c0e5b1326bf15a13bec76b8856af263d1fc5a716
James Sandy <james.sandy@metaswitch.com>
2023-12-16 07:30:46 +0000

    Day 12 part 1

--- src/day12.rs
+++ src/day12.rs
@@ -1,28 +1,50 @@
 use crate::Solution;
+use regex::Regex;
 
 #[derive(Clone, Debug)]
 pub struct Day12;
 
 impl Solution for Day12 {
-    type ParsedInput = Vec<Vec<char>>;
+    type ParsedInput = Vec<(Vec<char>, Vec<usize>)>;
 
     // Todo - this is wrong.
     // Use regex
-    fn parse_input(input: &str) -> Self::ParsedInput {
-        let input_lines = input
-            .lines()
-            .map(|x| x.to_string())
-            .collect::<Vec<String>>();
 
-        input_lines
-            .iter()
-            .map(|x| parse_input_line(x))
-            .collect::<Vec<Vec<char>>>()
+    fn parse_input(input: &str) -> Self::ParsedInput {
+        // let input_lines = input
+        //     .lines()
+        //     .map(|x| x.to_string())
+        //     .collect::<Vec<String>>();
+
+        // input_lines
+        //     .iter()
+        //     .map(|x| parse_row(x))
+        //     .collect::<Vec<Vec<char>>>()
+
+        let mut parsed_input = vec![];
+
+        let re = Regex::new(r"(?<row>[?#.]+) (?<groups>[0-9,]+)").unwrap();
+
+        for cap in re.captures_iter(input) {
+            let row = cap.name("row").unwrap().as_str();
+            let row = parse_row(&row);
+
+            let groups = cap.name("groups").unwrap().as_str();
+            let groups = parse_groups(&groups);
+
+            parsed_input.push((row, groups));
+        }
+
+        parsed_input
     }
 
     fn part_one(_parsed_input: &mut Self::ParsedInput) -> String {
-        // TODO: implement part one
-        0.to_string()
+        let sum: usize = _parsed_input
+            .iter()
+            .map(|(row, groups)| get_arrangements(row, groups))
+            .sum();
+
+        sum.to_string()
     }
 
     fn part_two(_parsed_input: &mut Self::ParsedInput) -> String {
@@ -31,8 +53,14 @@ impl Solution for Day12 {
     }
 }
 
-pub fn parse_input_line(input_line: &str) -> Vec<char> {
+pub fn parse_row(input_line: &str) -> Vec<char> {
     input_line.chars().collect()
+}
+pub fn parse_groups(input_group: &str) -> Vec<usize> {
+    input_group
+        .split(",")
+        .map(|x| x.to_string().parse::<usize>().unwrap())
+        .collect()
 }
 
 pub fn get_contiguous_damaged_groups(input: &Vec<char>) -> Vec<usize> {
@@ -58,6 +86,67 @@ pub fn get_contiguous_damaged_groups(input: &Vec<char>) -> Vec<usize> {
     groups
 }
 
+pub fn get_arrangements(row: &Vec<char>, groups: &Vec<usize>) -> usize {
+    get_arrangements_recursive(groups, row, &vec![], 0)
+}
+
+fn get_arrangements_recursive(
+    expected_groups: &Vec<usize>,
+    remaining_row: &[char],
+    found_groups: &Vec<usize>,
+    current_group: usize,
+) -> usize {
+    if !expected_groups.starts_with(&found_groups) {
+        0
+    } else if remaining_row.is_empty() {
+        let mut all_found_groups = found_groups.clone();
+        if current_group != 0 {
+            all_found_groups.push(current_group);
+        }
+
+        if expected_groups == &all_found_groups {
+            1
+        } else {
+            0
+        }
+    } else {
+        let (current_spring, remaining_row) = remaining_row.split_first().unwrap();
+        match current_spring {
+            '#' => get_arrangements_recursive(
+                expected_groups,
+                remaining_row,
+                found_groups,
+                current_group + 1,
+            ),
+            '.' => {
+                let mut new_found_group = found_groups.clone();
+                if current_group != 0 {
+                    new_found_group.push(current_group);
+                }
+                get_arrangements_recursive(expected_groups, remaining_row, &new_found_group, 0)
+            }
+            '?' => {
+                let branch_1 = get_arrangements_recursive(
+                    expected_groups,
+                    remaining_row,
+                    found_groups,
+                    current_group + 1,
+                );
+
+                let mut new_found_group = found_groups.clone();
+                if current_group != 0 {
+                    new_found_group.push(current_group);
+                }
+                let branch_2 =
+                    get_arrangements_recursive(expected_groups, remaining_row, &new_found_group, 0);
+
+                branch_1 + branch_2
+            }
+            _ => panic!("Error unexpected spring {:?}", current_spring),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -65,8 +154,16 @@ mod tests {
     #[test]
     fn test_get_contiguous_damaged_groups_cas1() {
         assert_eq!(
-            get_contiguous_damaged_groups(&parse_input_line("#.#.###")),
+            get_contiguous_damaged_groups(&parse_row("#.#.###")),
             vec![1, 1, 3]
+        )
+    }
+
+    #[test]
+    fn test_get_arrangements_cases_1() {
+        assert_eq!(
+            get_arrangements(&parse_row("???.###"), &parse_groups("1,1,3")),
+            1
         )
     }
 
